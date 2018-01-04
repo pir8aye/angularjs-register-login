b882f7695b253da38b43ca7fbdeff47b26bab564
andrewdyer <andrewdyer@10.0.0.11>
2018-01-04 20:08:18 +0000

    Run on port 3000 by default

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('*', (req, res) => {
 
 // 
 const server = require('http').createServer(app);
-server.listen(process.env.PORT || 8082, () => console.log('Express server listening on port %d', server.address().port));
+server.listen(process.env.PORT || 3000, () => console.log('Express server listening on port %d', server.address().port));
 
 // 
 const io = require('socket.io')(server);
